ffaa2849336ec9a3fea509f0c1f89296753d201f
gorleprasanth <gorleprasanth520@gmail.com>
2026-02-12 20:45:45 +0530

    fix: correct Firebase storageBucket domain

--- src/firebase.js
+++ src/firebase.js
@@ -6,7 +6,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyBEguuEbNwep7-Wqz42Cq5Fd8ej1ByQDZg",
   authDomain: "fileteluss-app.firebaseapp.com",
   projectId: "fileteluss-app",
-  storageBucket: "fileteluss-app.firebasestorage.app",
+  storageBucket: "fileteluss-app.appspot.com",
   messagingSenderId: "278776264027",
   appId: "1:278776264027:web:1aa6fb2a3d34a2dedf567e"
 };
